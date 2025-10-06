cb79900188eac58d64b1ed811dad88a67995905e
IrfanGalib <workwithirfanc@gmail.com>
2025-10-07 01:38:50 +0600

    Added the Navbar, Hero Section and Footer

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import BookImage from "../../assets/books.jpg";
+
+const Banner = () => {
+  return (
+    <div className="flex justify-between my-6 items-center p-16 w-full bg-gray-300 rounded-3xl">
+      <div className="space-y-4">
+        <h1 className="text-5xl font-bold">
+          Books to freshen up your bookshelf
+        </h1>
+        <button className="btn btn-active btn-primary">View The List</button>
+      </div>
+      <div>
+        <img className="rounded-2xl" src={BookImage} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/Pages/Books/Books.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from "react";
+
+const Books = () => {
+  const [allBooks, setAllBooks] = useState([]);
+
+  useEffect(() => {
+    fetch("booksData.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setAllBooks(data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Books</h1>
+    </div>
+  );
+};
+
+export default Books;
